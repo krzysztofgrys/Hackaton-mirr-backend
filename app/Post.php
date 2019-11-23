e3cb02c8e766b018723f5a45813a3f2ae52e461f
Łukasz Antończyk <lukasz.antonczyk@fingo.pl>
2019-11-23 16:58:50 +0100

    Add medialibrary

--- app/Post.php
+++ app/Post.php
@@ -3,9 +3,13 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Spatie\MediaLibrary\HasMedia\HasMedia;
+use Spatie\MediaLibrary\HasMedia\HasMediaTrait;
 
-class Post extends Model
+class Post extends Model implements HasMedia
 {
+    use HasMediaTrait;
+
     public function address(): \Illuminate\Database\Eloquent\Relations\BelongsTo
     {
         return $this->belongsTo(Address::class);
