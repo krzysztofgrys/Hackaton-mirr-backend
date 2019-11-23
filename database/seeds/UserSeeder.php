af33f73ae91af145038db27476f998893455a68b
Łukasz Antończyk <lukasz.antonczyk@fingo.pl>
2019-11-23 14:59:26 +0100

    Add User seeder

---
+++ database/seeds/UserSeeder.php
@@ -0,0 +1,33 @@
+<?php
+
+use App\Address;
+use App\User;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $address = Address::firstOrCreate([
+            'city' => 'Wrocław',
+            'zip_code' => '53-030',
+            'street' => 'Pl Powstańców Śląskich',
+            'house_number' => '7',
+            'coordinates' => new \Grimzy\LaravelMysqlSpatial\Types\Point(51.21, 17.2),
+        ]);
+        User::firstOrCreate([
+            'email' => 'user@bezinteresowni.pl',
+            'password' => 'useruser',
+            'first_name' => 'przykładowy',
+            'last_name' => 'użytkownik',
+            'date_of_birth' => new \DateTime('23-11-1969'),
+            'phone_number' => phone('123555789'),
+            'addresses_id' => $address->id,
+        ]);
+    }
+}
