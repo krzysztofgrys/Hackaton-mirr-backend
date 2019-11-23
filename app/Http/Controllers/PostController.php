36a45835c373fc8c9109a1553b0b8e0bed0738a7
Łukasz Antończyk <lukasz.antonczyk@fingo.pl>
2019-11-23 17:00:26 +0100

    Remove unnecessary posts routes

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -21,16 +21,6 @@ class PostController extends Controller
     }
 
     /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        abort(404);
-    }
-
-    /**
      * Store a newly created resource in storage.
      *
      * @param \Illuminate\Http\Request $request
@@ -82,39 +72,5 @@ class PostController extends Controller
     public function show(Post $post)
     {
         return $post;
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param \App\Post $post
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Post $post)
-    {
-        abort(404);
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param \Illuminate\Http\Request $request
-     * @param \App\Post $post
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, Post $post)
-    {
-        abort(404);
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param \App\Post $post
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(Post $post)
-    {
-        abort(404);
     }
 }
